b53df63d6477b151207ca4b99feffaaed0d32873
JD Lafrance <jdavid.lafrance@gmail.com>
2016-08-08 21:06:11 +0000

    more functional

--- app.js
+++ app.js
@@ -1,35 +1,42 @@
 var mongo = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
 var express = require('express');
+var url = require('url');
 var app = express();
-var urlM = 'mongodb://jd:admin@ds145295.mlab.com:45295/voting-polls';
 
+app.use(express.static('public'))
 app.set('view engine', 'pug')
 
+
+
+var urlM = 'mongodb://jd:admin@ds145295.mlab.com:45295/voting-polls';
 
 mongo.connect(urlM, function(err, db){
     if (err) console.log('Cannot connect');
     
-    db.collection('attempt').find({}, {title:1}).toArray(function(err, docs){
+    db.collection('attempt').find().toArray(function(err, docs){
         if (err) throw err;
-        app.get('/', function(req, res){
-            
+        
+            var ids = [];
             var values = [];
-            
             docs.forEach(function(val){
                 values.push(val.title);
+                ids.push(val._id)
             })
             
-            res.render('index', {title: 'VotingApp', message: 'Sign in to create a poll for you and your friends!', message1: 'Or select a poll below to see the results and vote.', values: values})
+        app.get('/', function(req, res){
+            res.render('index.pug', {title: 'VotingApp', values: values, display: ids});
             
-            function display(){
-                console.log('hi!!!')
-            }
-    
+})
 })
 
+app.get('/5*', function(req, res){
+    var str = req.url.substring(1)
+     db.collection('attempt').findOne({_id:ObjectId(str)}).then(function(result){
+       res.render('displayPoll.pug', {message: "Please vote if you have not already done so!",title:result.title, choices: result.choices.map(function(val){return val[0]}), votes: result.choices.map(function(val){return val[1]})})
     })
-    
 })
 
-
+})
+    
 app.listen(process.env.PORT);
